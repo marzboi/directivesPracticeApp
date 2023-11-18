cb6de7757e7ea05f372703717b968408198d980f
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-18 17:27:32 +0100

    Saving and ending practice project

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,9 +4,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ProductPageComponent],
-  imports: [CommonModule, ProductsRoutingModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ProductsRoutingModule,
+    ReactiveFormsModule,
+    SharedModule,
+  ],
 })
 export class ProductsModule {}
