743963cf3833841d6d58d433a2d0cba8d73a8b84
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-19 17:46:33 +0100

    Saving local changes

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { UserServiceService } from '../../services/user-service.service';
 
 @Component({
   templateUrl: './user-info-page.component.html',
-  styleUrl: './user-info-page.component.css'
+  styleUrl: './user-info-page.component.css',
 })
 export class UserInfoPageComponent {
+  private usersService = inject(UserServiceService);
 
+  public userId = signal(1);
 }
