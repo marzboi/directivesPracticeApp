90907f5370f597af4bbaf66925d5bd59f9f6a958
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2023-11-20 19:19:29 +0100

    Saving

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
 @Component({
@@ -14,5 +14,41 @@ export class PropertiesPageComponent {
     avatar: 'https://reqres.in/img/faces/1-image.jpg',
   });
 
-  onFieldUpdate(field: string, value: string) {}
+  public fullName = computed(
+    () => `${this.user().first_name} ${this.user().last_name}`
+  );
+
+  onFieldUpdate(field: keyof User, value: string) {
+    // this.user.set({
+    //   ...this.user(),
+    //   [field]: value,
+    // });
+
+    // this.user.update(current => ({
+    //   ...current,
+    //   [field]: value
+    // }))
+
+    this.user.update((current) => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return current;
+    });
+  }
 }
